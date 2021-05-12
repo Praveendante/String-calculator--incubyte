534b2adc5a411436dee1b9f650f50e010d543cd1
Praveendante <45708206+Praveendante@users.noreply.github.com>
2021-05-12 12:43:03 +0530

    Update StringCalculator_Incubyte.java
    Update:
    Code optimization is done and complexity reduced.

--- StringCalculator_Incubyte.java
+++ StringCalculator_Incubyte.java
@@ -22,58 +22,33 @@ public class StringCalculator_Incubyte {
 
     public static String add(String p){
         //counter++;
-        GetCalledCount();
+       GetCalledCount();
+        int a=0;
+        String s="";
+        ArrayList<String> t= new ArrayList<>();
+        List<Integer> negatives=new ArrayList<>();
         StringBuilder string1 = new StringBuilder(p);
         for(int i=0;i<p.length();i++){
             if(p.charAt(i)!='0' && p.charAt(i)!='1' && p.charAt(i)!='2'  && p.charAt(i)!='3' && p.charAt(i)!='4' && p.charAt(i)!='5' && p.charAt(i)!='6' && p.charAt(i)!='7' && p.charAt(i)!='8' && p.charAt(i)!='9' && p.charAt(i)!='-'){
                 string1.setCharAt(i,',');
             }
         }
-        String m=string1.toString();
-        String op=m.replaceAll(","," ");
-        String[] poo=op.split(" ");
-        ArrayList<String> pa=new ArrayList<>();
-        ArrayList<String> t= new ArrayList<>();
-        for(int i=0;i<poo.length;i++){
-            pa.add(poo[i]);
-        }
-        pa.forEach(w->{
-            if(!w.equals("") && Integer.parseInt(w)<1000){
-                t.add(w);
-            }
-        });
-        //negative cases
-        //ArrayStoreException e=new ArrayStoreException();
-        List<Integer> negatives=new ArrayList<>();
-        t.forEach(w->{
-            if(Integer.parseInt(w)<0){
-                negatives.add(Integer.parseInt(w));
-            }
-        });
-        int a=0;
-        String s="";
-        //try {
-        if(negatives.size()>0){
+        String[] poo=string1.toString().replaceAll(","," ").split(" ");
 
-            //   StringBuilder negativs = new StringBuilder();
-            ArrayList<Integer> negi=new ArrayList<>();
-            negatives.forEach(n -> {
-                   /* if(negatives.size()>1){
-                    negativs.append(n);
-                    negativs.append(",");}
-                    else {
-                        negativs.append(n);
-                    }*/
-                negi.add(n);
-            });
-            //   negativs.toString();
-            //  throw new ArrayStoreException("Negative is Not Allowed! given negative numbers are " + negativs.toString());
-            s="Negative is Not Allowed! given negative numbers are " + negi;
+        for(int i=0;i<poo.length;i++){
+            if(!poo[i].equals("") && !poo[i].equals("-") && Integer.parseInt(poo[i])<1000 && Integer.parseInt(poo[i])>0){
+                t.add(poo[i]);
+            }
+            else if(!poo[i].equals("")  && !poo[i].equals("-") && Integer.parseInt(poo[i])<0 ){
+                negatives.add(Integer.parseInt(poo[i]));
+            }
+        }
+        if(negatives.size()>0){
+            s="Negative is Not Allowed! given negative numbers are " + negatives;
         }
         else{
 
             a= t.stream().mapToInt(Integer::parseInt).sum();
-            //return a;
             s=""+a;
         }
         return s;
